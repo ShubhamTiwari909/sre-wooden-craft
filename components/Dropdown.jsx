5c3338dfe75b85c08762aad3731286c243729f72
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2024-04-30 14:57:01 +0530

    Added header along with shadcn and react icons installation

---
+++ components/Dropdown.jsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { FaChevronDown } from "react-icons/fa";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Dropdown = ({data}) => {
+  const pathname = usePathname();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger><FaChevronDown className="text-slate-500" /></DropdownMenuTrigger>
+      <DropdownMenuContent className="max-h-[200px] overflow-auto bg-slate-100">
+        {
+          data.map((item, index) => (
+            <DropdownMenuItem key={index.label}>
+              {item.link ? <Link href={item.value} className={pathname === item.value ? "text-blue-700" : ""}>{item.label}</Link> : <span>{item.label}</span>}
+            </DropdownMenuItem>
+          ))
+        }
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default Dropdown;
---
+++ components/Header.jsx
@@ -0,0 +1,158 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/input";
+import { AiOutlineSearch } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiCloseCircleFill } from "react-icons/ri";
+import Dropdown from "./Dropdown";
+
+const categories = [
+  {
+    value: "/category1",
+    label: "Category 1",
+    link: true,
+  },
+  {
+    value: "/category2",
+    label: "Category 2",
+    link: true,
+  },
+  {
+    value: "/category3",
+    label: "Category 3",
+    link: true,
+  },
+  {
+    value: "/category4",
+    label: "Category 4",
+    link: true,
+  },
+  {
+    value: "/category5",
+    label: "Category 5",
+    link: true,
+  },
+  {
+    value: "/category6",
+    label: "Category 6",
+    link: true,
+  },
+  {
+    value: "/category7",
+    label: "Category 7",
+    link: true,
+  },
+];
+
+const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const [search, setSearch] = useState("");
+  const pathname = usePathname();
+
+  return (
+    <header className="flex border-b py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide fixed top-0 w-full z-100">
+      <div className="flex flex-wrap items-center gap-5 w-full">
+        <Link href="#">
+          <img
+            src="https://readymadeui.com/readymadeui.svg"
+            alt="logo"
+            className="w-36"
+          />
+        </Link>
+        <div
+          id="collapseMenu"
+          className={`${
+            openMenu ? "" : "max-lg:-translate-x-full"
+          } lg:!block max-lg:w-full max-lg:fixed max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50`}
+        >
+          <Button
+            onClick={() => setOpenMenu(!openMenu)}
+            id="toggleClose"
+            className="lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white p-3"
+          >
+            <RiCloseCircleFill size="1.5rem" color="#000000"/>
+          </Button>
+
+          <ul
+            className={`${
+              openMenu ? "" : "max-lg:-translate-x-full"
+            } lg:flex lg:ml-14 lg:gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50 transition-all duration-300 ease-in-out`}
+          >
+            <li className="mb-6 hidden max-lg:block">
+              <Link href="#">
+                <img
+                  src="https://readymadeui.com/readymadeui.svg"
+                  alt="logo"
+                  className="w-36"
+                />
+              </Link>
+            </li>
+            <li className="max-lg:border-b max-lg:py-3 px-3">
+              <Link
+                href="/"
+                onClick={() => setOpenMenu(!openMenu)}
+                className={`lg:hover:text-[#007bff] ${pathname === "/" ? "text-[#007bff]" : "text-gray-500"} block font-semibold text-[15px]`}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="max-lg:border-b max-lg:py-3 px-3 flex gap-2">
+              <Link
+                href="/categories"
+                onClick={() => setOpenMenu(!openMenu)}
+                className={`lg:hover:text-[#007bff] ${pathname === "/categories" ? "text-[#007bff]" : "text-gray-500"} block font-semibold text-[15px]`}
+              >
+                Categories
+              </Link>
+              <Dropdown data={categories} />
+            </li>
+            <li className="max-lg:border-b max-lg:py-3 px-3">
+              <Link
+                href="/contact"
+                onClick={() => setOpenMenu(!openMenu)}
+                className={`lg:hover:text-[#007bff] ${pathname === "/contact" ? "text-[#007bff]" : "text-gray-500"} block font-semibold text-[15px]`}
+              >
+                Contact
+              </Link>
+            </li>
+            <li className="max-lg:border-b max-lg:py-3 px-3">
+              <Link
+                href="/about"
+                onClick={() => setOpenMenu(!openMenu)}
+                className={`lg:hover:text-[#007bff] ${pathname === "/about" ? "text-[#007bff]" : "text-gray-500"} block font-semibold text-[15px]`}
+              >
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <Button
+          id="toggleOpen"
+          onClick={() => setOpenMenu(!openMenu)}
+          className="lg:hidden ml-auto"
+        >
+          <GiHamburgerMenu size="1.5rem" />
+        </Button>
+
+        <div className="flex lg:ml-auto max-lg:w-full">
+          <div className="flex gap-4 items-center xl:w-80 max-xl:w-full bg-slate-100 px-6 py-3 rounded outline outline-transparent">
+            <Input
+              type="text"
+              placeholder="Search..."
+              className="focus-visible:ring-blue-600"
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+            />
+            <AiOutlineSearch size="1.5rem" color="darkgrey" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
